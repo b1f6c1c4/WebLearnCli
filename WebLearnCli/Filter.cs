4cbef13aca0639ce0d260908625ad8d61b0620b6
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-12-27 15:20:20 +0800

    filter (not finished)

--- WebLearnCli/Filter.cs
+++ WebLearnCli/Filter.cs
@@ -5,8 +5,20 @@ using WebLearnCore;
 
 namespace WebLearnCli
 {
-    public static class Filter
+    public class Filter
     {
+        private readonly string m_Lesson;
+        private readonly bool m_Announcement;
+        private readonly bool m_Document;
+        private readonly bool m_Assignment;
+        private readonly string m_Title;
+        private readonly int m_Index;
+
+        public static IEnumerable<Lesson> GetLessons(bool previous = false, bool noCurrent = false) =>
+            Config.Inst.Lessons
+                  .Where(l => previous || l.Term == TermInfo.Current)
+                  .Where(l => !noCurrent || l.Term != TermInfo.Current);
+
         public static Lesson GetLesson(string str, IReadOnlyCollection<Lesson> raw)
         {
             var lst = raw
@@ -32,7 +44,9 @@ namespace WebLearnCli
                 s = Console.ReadLine();
                 int id;
                 if (int.TryParse(s, out id))
-                    return lst[id];
+                    if (id >= 0 &&
+                        id < lst.Count)
+                        return lst[id];
 
                 if (s == "r" ||
                     s == "R")
@@ -44,9 +58,114 @@ namespace WebLearnCli
             return GetLesson(s, raw);
         }
 
-        public static IEnumerable<Lesson> GetLessons(bool previous = false, bool noCurrent = false) =>
-            Config.Inst.Lessons
-                  .Where(l => previous || l.Term == TermInfo.Current)
-                  .Where(l => !noCurrent || l.Term != TermInfo.Current);
+        public bool IsMatch(Lesson lesson)
+        {
+            if (m_Lesson == "*")
+                return true;
+
+            return lesson.Name == m_Lesson || lesson.Alias.Contains(m_Lesson);
+        }
+
+        public IEnumerable<Assignment> Filt(List<Assignment> objs)
+        {
+            if (!m_Assignment)
+                return Enumerable.Empty<Assignment>();
+
+            return Filt(objs, o => o.Title);
+        }
+
+        private IEnumerable<T> Filt<T>(IReadOnlyList<T> objs, Func<T, string> title)
+        {
+            if (m_Title != null)
+            {
+                var obj = objs.SingleOrDefault(o => title(o).Trim() == m_Title);
+                yield return obj;
+                yield break;
+            }
+
+            if (m_Index == -1)
+            {
+                yield return objs[m_Index];
+                yield break;
+            }
+
+            foreach (var obj in objs)
+                yield return obj;
+        }
+
+        public Filter(string path)
+        {
+            var t = path.Trim();
+            var lid = t.IndexOf('/');
+            if (lid == 0)
+                // deadline shorthand
+                throw new NotImplementedException();
+
+            if (lid < 0)
+            {
+                m_Lesson = t;
+                return;
+            }
+
+            m_Lesson = t.Substring(0, lid);
+            t = t.Substring(lid + 1);
+            if (string.IsNullOrWhiteSpace(t))
+                throw new FormatException();
+
+            if (t == "*")
+            {
+                m_Announcement = true;
+                m_Document = true;
+                m_Assignment = true;
+                m_Title = null;
+                m_Index = -1;
+                return;
+            }
+
+            if (int.TryParse(t, out m_Index))
+            {
+                m_Announcement = false;
+                m_Document = false;
+                m_Assignment = true;
+                m_Title = null;
+                return;
+            }
+
+            if (t.StartsWith("a", StringComparison.OrdinalIgnoreCase))
+            {
+                m_Announcement = true;
+                m_Document = false;
+                m_Assignment = false;
+            }
+            else if (t.StartsWith("f", StringComparison.OrdinalIgnoreCase))
+            {
+                m_Announcement = false;
+                m_Document = true;
+                m_Assignment = false;
+            }
+            else if (t.StartsWith("d", StringComparison.OrdinalIgnoreCase))
+            {
+                m_Announcement = false;
+                m_Document = false;
+                m_Assignment = true;
+            }
+            else
+                throw new FormatException(path);
+
+            if (t[1] != '/')
+                throw new FormatException(path);
+
+            t = t.Substring(2);
+
+
+            if (int.TryParse(t, out m_Index))
+            {
+                m_Title = null;
+                return;
+            }
+
+            m_Title = t.Trim();
+            m_Index = -1;
+        }
     }
 }
