540c9947bd2d194334915724acf2476637f282e9
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-12-29 21:43:49 +0800

    show

--- WebLearnCli/ShowCommand.cs
+++ WebLearnCli/ShowCommand.cs
@@ -19,38 +19,59 @@ namespace WebLearnCli
         protected override int ConcreteRun(Lesson lesson, LessonExtension ext, IEnumerable<Announcement> objs)
         {
             var lst = objs.ToList();
-            if (ext.Announcements.Count == lst.Count)
-                for (var i = 0; i < lst.Count; i++)
-                {
-                    var obj = lst[i];
-                    Console.WriteLine("==================================");
-                    Console.WriteLine($"[{i,3}] {lesson.Term} {lesson.Name}/a/{obj.Title}");
-                    Console.WriteLine($"Date: {obj.Date:yyyyMMdd} From: {obj.From}");
-                    Console.WriteLine(obj.Content);
-                    Console.WriteLine();
-                }
-            else
-            // ReSharper disable once ForCanBeConvertedToForeach
-                for (var i = 0; i < lst.Count; i++)
-                {
-                    var obj = lst[i];
-                    Console.WriteLine("==================================");
-                    Console.WriteLine($"[   ] {lesson.Term} {lesson.Name}/a/{obj.Title}");
-                    Console.WriteLine($"Date: {obj.Date:yyyyMMdd} From: {obj.From}");
-                    Console.WriteLine(obj.Content);
-                    Console.WriteLine();
-                }
-            return 1;
+            for (var i = 0; i < lst.Count; i++)
+            {
+                var obj = lst[i];
+                Console.WriteLine("==================================");
+                Console.Write(ext.Announcements.Count == lst.Count ? $"[{i,3}]" : "[   ]");
+                Console.WriteLine($" {lesson.Term} {lesson.Name}/a/{obj.Title}");
+                Console.WriteLine($"Date: {obj.Date:yyyyMMdd} From: {obj.From}");
+                Console.WriteLine(obj.Content);
+                Console.WriteLine();
+            }
+            return 0;
         }
 
         protected override int ConcreteRun(Lesson lesson, LessonExtension ext, IEnumerable<Document> objs)
         {
-            throw new NotImplementedException();
+            var lst = objs.ToList();
+            for (var i = 0; i < lst.Count; i++)
+            {
+                var obj = lst[i];
+                Console.WriteLine("==================================");
+                Console.Write(ext.Documents.Count == lst.Count ? $"[{i,3}]" : "[   ]");
+                Console.WriteLine($" {lesson.Term} {lesson.Name}/a/{obj.Title}");
+                Console.WriteLine($"Date: {obj.Date:yyyyMMdd}");
+                Console.WriteLine($"File: {obj.FileSize,20:N0}B {obj.FileName}");
+                Console.WriteLine(obj.Abstract);
+                Console.WriteLine();
+            }
+            return 0;
         }
 
         protected override int ConcreteRun(Lesson lesson, LessonExtension ext, IEnumerable<Assignment> objs)
         {
-            throw new NotImplementedException();
+            var lst = objs.ToList();
+            for (var i = 0; i < lst.Count; i++)
+            {
+                var obj = lst[i];
+                Console.Write("==================================");
+                if (obj.IsIgnored)
+                    Console.WriteLine();
+                else if (!obj.IsSubmitted)
+                    Console.WriteLine("[NOT SUBMITTED]");
+                else
+                    Console.WriteLine("[SUBMITTED]");
+                Console.Write(ext.Assignments.Count == lst.Count ? $"[{i,3}]" : "[   ]");
+                Console.WriteLine($" {lesson.Term} {lesson.Name}/a/{obj.Title}");
+                Console.WriteLine($"Date: {obj.Date:yyyyMMdd}");
+                Console.WriteLine($"Due:  {obj.DueDate:yyyyMMdd}");
+                Console.WriteLine($"File: {obj.FileSize,20:N0}B {obj.FileName}");
+                Console.WriteLine($"{obj.Score,10} {obj.Assess}");
+                Console.WriteLine(obj.Content);
+                Console.WriteLine();
+            }
+            return 0;
         }
     }
 }
