5af2cc379b6d9ecac6c8498b37b624b6d7dd0726
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-05-13 12:45:57 +0800

    fix bug

--- WebLearnCli/Formatter.cs
+++ WebLearnCli/Formatter.cs
@@ -14,12 +14,12 @@ namespace WebLearnCli
             if (remain.TotalDays >= 2)
                 return $"{ddl.DueDate:yyyyMMdd} + {remain.Days,11} days  {ddl.Name}/d/{ddl.Title}";
             if (remain.TotalHours >= 24)
-                return $"{ddl.DueDate:yyyyMMdd} +++ {remain.Hours,9} hours {ddl.Name}/d/{ddl.Title}";
+                return $"{ddl.DueDate:yyyyMMdd} +++ {(int)remain.TotalHours,9} hours {ddl.Name}/d/{ddl.Title}";
             if (remain.TotalHours >= 2)
-                return $"{ddl.DueDate:yyyyMMdd} ++++ {remain.Hours,8} hours {ddl.Name}/d/{ddl.Title}";
-            if (remain.Minutes >= 2)
-                return $"{ddl.DueDate:yyyyMMdd} +++++++ {remain.Minutes,5} mins  {ddl.Name}/d/{ddl.Title}";
-            return $"{ddl.DueDate:yyyyMMdd} ++++++++ {remain.Seconds,4} secs  {ddl.Name}/d/{ddl.Title}";
+                return $"{ddl.DueDate:yyyyMMdd} ++++ {(int)remain.TotalHours,8} hours {ddl.Name}/d/{ddl.Title}";
+            if (remain.TotalMinutes >= 2)
+                return $"{ddl.DueDate:yyyyMMdd} +++++++ {(int)remain.TotalMinutes,5} mins  {ddl.Name}/d/{ddl.Title}";
+            return $"{ddl.DueDate:yyyyMMdd} ++++++++ {(int)remain.TotalSeconds,4} secs  {ddl.Name}/d/{ddl.Title}";
         }
     }
 }
